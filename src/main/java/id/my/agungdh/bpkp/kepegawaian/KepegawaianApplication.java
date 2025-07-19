5e13b43a333a5b14f1b8ddf838d74812c40689c2
AgungDH <agungdh@live.com>
2025-07-19 07:52:52 +0700

    init

---
+++ src/main/java/id/my/agungdh/bpkp/kepegawaian/KepegawaianApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.bpkp.kepegawaian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepegawaianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KepegawaianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/agungdh/bpkp/kepegawaian/KepegawaianApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.bpkp.kepegawaian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KepegawaianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
